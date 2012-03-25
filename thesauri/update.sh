acf5cd82ac9ef8342f6201f33bfdf1b764cbbd4b
Alexander Dutton <alexander.dutton@oucs.ox.ac.uk>
2012-03-25 00:26:00 +0000

    Added transformed SKOS definitions for attendanceMode, attendancePattern and studyMode, and a script to generate them using skosify-vdex.xsl.

---
+++ thesauri/update.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+( cd attendancePattern ;
+  curl http://xcri.co.uk/vocabularies/attendancePattern2_1.xml > attendancePattern.vdex ;
+  saxon attendancePattern.vdex ../skosify-vdex.xsl > attendancePattern.rdf )
+
+( cd attendanceMode ;
+  curl http://xcri.co.uk/vocabularies/attendanceMode2_1.xml > attendanceMode.vdex ;
+  saxon attendanceMode.vdex ../skosify-vdex.xsl > attendanceMode.rdf )
+
+( cd studyMode ;
+  curl http://xcri.co.uk/vocabularies/studyMode2_1.xml > studyMode.vdex ;
+  saxon studyMode.vdex ../skosify-vdex.xsl > studyMode.rdf )
+
