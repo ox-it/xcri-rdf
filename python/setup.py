a501ec1b29350318043e2a1b895bc251ef86785d
Alexander Dutton <alexander.dutton@oucs.ox.ac.uk>
2012-04-15 23:03:22 +0100

    Added a Python setup.py script for xcri_rdf.py

---
+++ python/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='xcri_rdf',
+    description='A module and script to convert RDF about courses into XCRI-CAP 1.2 XML.',
+    author='Oxford University Computing Services',
+    author_email='opendata@oucs.ox.ac.uk',
+    version='0.1',
+    license='BSD',
+    py_modules=['xcri_rdf'],
+)
